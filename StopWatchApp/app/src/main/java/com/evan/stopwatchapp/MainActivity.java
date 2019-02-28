0094e22b188e56893385b68eecfe9fbe0cd5e881
Evan Maroge <evanmaroge@Evans-MacBook-Pro.local>
2019-02-28 16:39:38 -0500

    Some bug fixes.

--- StopWatchApp/app/src/main/java/com/evan/stopwatchapp/MainActivity.java
+++ StopWatchApp/app/src/main/java/com/evan/stopwatchapp/MainActivity.java
@@ -12,44 +12,50 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private Button start, stop, reset;
-    private TextView timer;
 
-    private int seconds =0;
+
+    private int seconds = 0;
     private boolean running;
     private boolean wasRunning;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(savedInstanceState!=null){
-            seconds= savedInstanceState.getInt("seconds");
-            running = savedInstanceState.getBoolean("running");
-           wasRunning=  savedInstanceState.getBoolean("wasRunning");
 
-            Log.d("true or false", "not running" +running);
+        //the the values were saved when there was a configuration change, restore them...
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+
+            Log.d("true or false", "not running" + running);
             Log.d("sec", "not running" + seconds);
 
 
-            if(running){
+            if (running) {
                 startTimer();
             }
             Log.d("destroyed", "not null act");
 
         }
 
-        start = (Button)findViewById(R.id.startButton);
-        stop = (Button)findViewById(R.id.stopButton);
-        reset = (Button)findViewById(R.id.resetButton);
+        start = (Button) findViewById(R.id.startButton);
+        stop = (Button) findViewById(R.id.stopButton);
+        reset = (Button) findViewById(R.id.resetButton);
 
-        timer = (TextView) findViewById(R.id.timer);
+//        timer = (TextView) findViewById(R.id.timer);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startTimer();
+                if(!running){
+                    startTimer();
+
+                }
+
             }
         });
-
 
 
         stop.setOnClickListener(new View.OnClickListener() {
@@ -78,11 +84,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        if(wasRunning){
+        if (wasRunning) {
             running = wasRunning;
         }
     }
-
 
 
     //called after on stop be4 destroyed
@@ -92,50 +97,52 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("destroyed", "destroyed act");
         //save values
-        outState.putInt("seconds",seconds);
-        outState.putBoolean("running",running);
-        outState.putBoolean("wasRunning",wasRunning);
+        outState.putInt("seconds", seconds);
+        outState.putBoolean("running", running);
+        outState.putBoolean("wasRunning", wasRunning);
 
     }
 
 
-
-
     private void startTimer() {
         running = true;
+         final TextView timer = (TextView) findViewById(R.id.timer);
 
 
-        final Handler handler= new Handler();
+        final Handler handler = new Handler();
         //use post method to avoid delay
-        handler.post(new Runnable(){
+        handler.post(new Runnable() {
             @Override
             public void run() {
-                int hours = seconds/3600;
-                int minutes = (seconds%3600)/60;
-                int secs = seconds%50;
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
                 //hours  minutes and seconds format
-                String time = String.format(Locale.getDefault(),"%d:%2d:%02d", hours, minutes,secs);
+                String time = String.format(Locale.getDefault(), "%d:%2d:%02d", hours, minutes, secs);
 
                 timer.setText(time);
 
-                if(running){seconds++;}
+                if (running) {
+                    seconds++;
+                }
                 handler.postDelayed(this, 1000);
             }
         });
 
     }
+
     private void stopTimer() {
         running = false;
 
+
     }
+
     private void resetTimer() {
         running = false;
         seconds = 0;
 
 
     }
-
-
 
 
 }
