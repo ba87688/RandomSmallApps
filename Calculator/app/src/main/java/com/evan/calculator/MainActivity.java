ce2ae55b6ea93fa3bb84b59437bee0f757db7d8e
Evan Maroge <evanmaroge@Evans-MacBook-Pro.local>
2019-02-28 15:43:57 -0500

    Some bug fixes.

--- Calculator/app/src/main/java/com/evan/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/evan/calculator/MainActivity.java
@@ -11,182 +11,201 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView userInput, resuts;
-    Boolean add = false;
-    Boolean minus = false;
-    Boolean divide = false;
-    Boolean multiply = false;
-    Boolean selected = false;
+    private Boolean add = false;
+    private Boolean minus = false;
+    private Boolean divide = false;
+    private Boolean multiply = false;
+    private Boolean selected = false;
 
-    String firstValue ="";
-    String secondValue ="";
+    private String firstValue = "";
+    private String secondValue = "";
 
-    float value1,value2;
+
+    private boolean gotvalue;
+    private float value1, value2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gotvalue = false;
 
         resuts = (TextView) findViewById(R.id.userResult);
-        userInput = (TextView)findViewById(R.id.inputOfUser);
-
-
+        userInput = (TextView) findViewById(R.id.inputOfUser);
 
 
     }
-    public void process(View view){
-        button= (Button)view;
+
+    //method called when the buttons are pressed
+    public void process(View view) {
+        //cast the view to button
+        button = (Button) view;
+        //get the string of the button
         String v = button.getText().toString();
-            if(v.equalsIgnoreCase(getResources().getString(R.string.zero))){
-                userInput.setText(userInput.getText()+getResources().getString(R.string.zero));
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.one))){
-                userInput.setText(userInput.getText()+getResources().getString(R.string.one));
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.two))){
+        if (v.isEmpty() || v == null) {
+            Log.d("Nothing", "process: put something in there. ");
+        } else {
+            //see what action user took and act accordingly...
+            if (v.equalsIgnoreCase(getResources().getString(R.string.zero))) {
+                gotvalue = true;
+                userInput.setText(userInput.getText() + getResources().getString(R.string.zero));
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.one))) {
+                gotvalue = true;
+                userInput.setText(userInput.getText() + getResources().getString(R.string.one));
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.two))) {
+                gotvalue = true;
+                userInput.setText(userInput.getText() + getResources().getString(R.string.two));
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.three))) {
+                gotvalue = true;
 
-                userInput.setText(userInput.getText()+getResources().getString(R.string.two));
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.three))){
+                userInput.setText(userInput.getText() + getResources().getString(R.string.three));
 
-                userInput.setText(userInput.getText()+getResources().getString(R.string.three));
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.four))) {
+                gotvalue = true;
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.four))){
-                userInput.setText(userInput.getText()+getResources().getString(R.string.four));
+                userInput.setText(userInput.getText() + getResources().getString(R.string.four));
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.five))){
-                userInput.setText(userInput.getText()+getResources().getString(R.string.five));
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.five))) {
+                gotvalue = true;
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.six))){
-                userInput.setText(userInput.getText()+getResources().getString(R.string.six));
+                userInput.setText(userInput.getText() + getResources().getString(R.string.five));
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.seven))){
-                userInput.setText(userInput.getText()+getResources().getString(R.string.seven));
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.six))) {
+                gotvalue = true;
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.eight))){
-                userInput.setText(userInput.getText()+getResources().getString(R.string.eight));
+                userInput.setText(userInput.getText() + getResources().getString(R.string.six));
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.nine))){
-                userInput.setText(userInput.getText()+getResources().getString(R.string.nine));
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.seven))) {
+                gotvalue = true;
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.add))){
-                //get value of first calculation
-                firstValue = userInput.getText().toString();
+                userInput.setText(userInput.getText() + getResources().getString(R.string.seven));
 
-                value1 = Float.parseFloat(userInput.getText() +"");
-                add = true;
-                userInput.setText(null);
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.eight))) {
+                gotvalue = true;
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.minus))){
-                //get value of first calculation
-                firstValue = userInput.getText().toString();
+                userInput.setText(userInput.getText() + getResources().getString(R.string.eight));
 
-                value1 = Float.parseFloat(userInput.getText() +"");
-                minus = true;
-                userInput.setText(null);
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.nine))) {
+                gotvalue = true;
+
+                userInput.setText(userInput.getText() + getResources().getString(R.string.nine));
+
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.add))) {
+                if (!gotvalue) {
+                    Log.d("main", "process: look");
+                } else {
+                    //get value of first calculation
+                    firstValue = userInput.getText().toString();
+
+                    value1 = Float.parseFloat(userInput.getText() + "");
+                    add = true;
+                    userInput.setText(null);
+                }
+
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.minus))) {
+                if (!gotvalue) {
+                    Log.d("main", "process: look");
+                } else {
+                    //get value of first calculation
+                    firstValue = userInput.getText().toString();
+
+                    value1 = Float.parseFloat(userInput.getText() + "");
+                    minus = true;
+                    userInput.setText(null);
+                }
+
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.multiply))) {
+                if (!gotvalue) {
+                    Log.d("main", "process: look");
+                } else {
+                    //get value of first calculation
+                    firstValue = userInput.getText().toString();
+
+                    value1 = Float.parseFloat(userInput.getText() + "");
+                    multiply = true;
+                    userInput.setText(null);
+                }
 
 
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.divide))) {
+                if (!gotvalue) {
+                    Log.d("main", "process: look");
+                } else {
+                    //get value of first calculation
+                    firstValue = userInput.getText().toString();
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.multiply))){
-                //get value of first calculation
-                firstValue = userInput.getText().toString();
+                    value1 = Float.parseFloat(userInput.getText() + "");
+                    divide = true;
+                    userInput.setText(null);
+                }
 
-                value1 = Float.parseFloat(userInput.getText() +"");
-                multiply = true;
-                userInput.setText(null);
-
-
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.divide))){
-                //get value of first calculation
-                firstValue = userInput.getText().toString();
-
-                value1 = Float.parseFloat(userInput.getText() +"");
-                divide = true;
-                userInput.setText(null);
-
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.C))){
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.C))) {
                 userInput.setText("");
                 resuts.setText("0");
-                add=false;
-                minus=false;
-                multiply=false;
-                divide=false;
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.percentage))){
-                if(userInput==null){
+                add = false;
+                minus = false;
+                multiply = false;
+                divide = false;
+                gotvalue = false;
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.percentage))) {
+                if (!gotvalue) {
+                    Log.d("main", "process: look");
+                } else {
+                    if (userInput == null) {
+                        userInput.setText("");
+                    } else {
+                        float temp = Float.parseFloat(userInput.getText() + "");
+                        temp = temp % 100;
+
+                        userInput.setText(temp + "");
+                    }
+                }
+
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.dot))) {
+                userInput.setText(userInput.getText() + getResources().getString(R.string.dot));
+
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.erase))) {
+                if (userInput == null) {
                     userInput.setText("");
-                }else {
-                    float temp = Float.parseFloat(userInput.getText() + "");
-                    temp = temp % 100;
-
-                    userInput.setText(temp + "");
+                } else {
+                    String f = userInput.getText() + getResources().getString(R.string.eight);
+                    f.substring(0, (f.length() - 1));
+                    userInput.setText(f.substring(0, (f.length() - 1)));
                 }
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.dot))){
-                userInput.setText(userInput.getText()+getResources().getString(R.string.dot));
+                //if the user pressed the equals button, you want to process results
+            } else if (v.equalsIgnoreCase(getResources().getString(R.string.equal))) {
+                if (userInput == null) {
 
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.erase))){
-                if(userInput==null){
-                    userInput.setText("");
-                }
-                else{
-                    String f = userInput.getText()+getResources().getString(R.string.eight);
-                      f.substring(0,(f.length()-1));
-                     userInput.setText(f.substring(0,(f.length()-1)));
-                }
-
-            }
-            else if(v.equalsIgnoreCase(getResources().getString(R.string.equal))){
-                if(userInput==null){
-
-                }
-                else {
+                } else {
                     value2 = Float.parseFloat(userInput.getText() + "");
                     if (add) {
                         resuts.setText(value1 + value2 + "");
                         add = false;
-                        userInput.setText(value1+value2+"");
+                        userInput.setText(value1 + value2 + "");
                     }
                     if (minus) {
                         resuts.setText(value1 - value2 + "");
                         minus = false;
-                        userInput.setText(value1 - value2 +"");
+                        userInput.setText(value1 - value2 + "");
 
                     }
                     if (multiply) {
                         resuts.setText(value1 * value2 + "");
                         multiply = false;
-                        userInput.setText(value1 * value2 +"");
+                        userInput.setText(value1 * value2 + "");
                     }
                     if (divide) {
                         resuts.setText(value1 % value2 + "");
                         divide = false;
-                        userInput.setText(value1 * value2 +"");
+                        userInput.setText(value1 * value2 + "");
                     }
                 }
 
 
             }
-
-
-
-
-
-
-
+        }
 
     }
 }
